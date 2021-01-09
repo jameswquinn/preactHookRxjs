0a9f0b61ac7de9ccce73e16522af9e0d151d48fe
jameswquinn <jameswquinn@users.noreply.github.com>
2021-01-09 21:58:46 +0000

    example useTransition from React-Spring

--- src/index.js
+++ src/index.js
@@ -3,10 +3,49 @@ import { useRef, useState, useEffect } from "preact/hooks";
 import { useObservable } from "rxjs-hooks";
 import { interval } from "rxjs";
 import { map } from "rxjs/operators";
-import { useSpring, animated } from "react-spring";
+import { useTransition, config, useSpring, animated } from "react-spring";
 import useMeasure from "./useMeasure";
 
 import "./styles.css";
+
+const slides = [
+  { id: 0, url: "photo-1551989137-334bd6577da3?ixlib=rb-1.2.1&w=3450&q=80" },
+  {
+    id: 1,
+    url:
+      "photo-1501560379-05951a742668?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=3300&q=80"
+  },
+  { id: 2, url: "photo-1551989137-b8ad7595d020?ixlib=rb-1.2.1&w=3300&q=80" },
+  {
+    id: 3,
+    url:
+      "photo-1503058474900-cb76710f9cd1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=3300&q=80"
+  }
+];
+
+const App3 = () => {
+  const [index, set] = useState(0);
+  const transitions = useTransition(slides[index], (item) => item.id, {
+    from: { opacity: 0, transform: "scale(1.1)" },
+    enter: { opacity: 1, transform: "scale(1)" },
+    leave: { opacity: 0, transform: "scale(0.9)" },
+    config: config.molasses
+  });
+  useEffect(
+    () => void setInterval(() => set((state) => (state + 1) % 4), 2000),
+    []
+  );
+  return transitions.map(({ item, props, key }) => (
+    <animated.div
+      key={key}
+      class="bg"
+      style={{
+        ...props,
+        backgroundImage: `url(https://images.unsplash.com/${item.url}&auto=format&fit=crop)`
+      }}
+    />
+  ));
+};
 
 function App2() {
   const [open, toggle] = useState(false);
@@ -40,5 +79,5 @@ function App() {
 }
 
 if (typeof window !== "undefined") {
-  render(<App2 />, document.getElementById("root"));
+  render(<App3 />, document.getElementById("root"));
 }
